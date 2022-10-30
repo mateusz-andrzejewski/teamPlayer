9fd0fa8fa8da82c7d5d5e8b4e08560e791465773
Mateusz Andrzejewski <mateuszaandrzejewski@gmail.com>
2022-10-30 11:16:14 +0100

    menu phase

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/core/sidebar/sidebar.component';
+import { HeaderComponent } from './components/core/header/header.component';
+import { SearchComponent } from './components/core/search/search.component';
+import { TitleComponent } from './components/core/title/title.component';
+import { NotFoundComponent } from './components/core/not-found/not-found.component';
+import { FooterComponent } from './components/core/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    SearchComponent,
+    TitleComponent,
+    NotFoundComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
